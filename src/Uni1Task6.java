8df8fcd8603b7dc461d1f89675f5e2f3954535ab
Kostya Kalevich <kostyadante@tut.by>
2021-07-09 17:50:17 +0300

    Finished Uni1Task6

--- src/Uni1Task6.java
+++ src/Uni1Task6.java
@@ -1,13 +1,13 @@
 public class Uni1Task6 {
-    public static void main (String[]args){
-        int a=86;
-        int b=-556756756;
-        if (a>b){
-            System.out.println(a+" Больше чем "+b);
-        }else if (b<a){
-            System.out.println(b+" Больше чем "+a);
-        }else{
-            System.out.println(a+" Равняется "+b);
+    public static void main(String[] args) {
+        int a = 86;
+        int b = -556756756;
+        if (a > b) {
+            System.out.println(a + " Больше чем " + b);
+        } else if (b < a) {
+            System.out.println(b + " Больше чем " + a);
+        } else {
+            System.out.println(a + " Равняется " + b);
         }
     }
 }
