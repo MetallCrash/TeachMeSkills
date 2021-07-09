cf0bb54a6fbc18b17235362b3e65786ab6f39307
Kostya Kalevich <kostyadante@tut.by>
2021-07-09 19:38:59 +0300

    Finished Uni1Task7

--- src/Uni1Task7.java
+++ src/Uni1Task7.java
@@ -1,12 +1,11 @@
 public class Uni1Task7 {
     public static void main(String[] args) {
         int a = 1001;
-        int b = 0;
-        b = a % 100;
+        int b = a % 100;
         if (b >= 10 && b <= 19) {
             System.out.println(a + " Программистов");
         } else {
-            b = a % 10;
+            b = b % 10;
             if (b == 0 || (b >= 5 && b <= 9)) {
                 System.out.println(a + " Программистов");
             } else if (b == 1) {
