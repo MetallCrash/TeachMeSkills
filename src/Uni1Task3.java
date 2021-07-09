96427d934deed70b26c4d6dc1f065f4d8dbaa8c0
Kostya Kalevich <kostyadante@tut.by>
2021-07-09 16:38:43 +0300

    Finished Uni1Task3

---
+++ src/Uni1Task3.java
@@ -0,0 +1,12 @@
+public class Uni1Task3 {
+    public static void main (String[]args){
+        int a=0;
+        if (a>0){
+            System.out.println("a="+(a+1));
+        }else if (a<0){
+            System.out.println("a="+(a-2));
+        }else{
+            System.out.println("a="+(a+10));
+        }
+    }
+}
