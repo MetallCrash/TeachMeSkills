9dfc547a0701f18a5bef6cc12a1444785e236d92
Kostya Kalevich <kostyadante@tut.by>
2021-07-09 16:53:14 +0300

    Finished Uni1Task2

--- src/Uni1Task2.java
+++ src/Uni1Task2.java
@@ -3,7 +3,7 @@ public class Uni1Task2 {
         int a = 5;
         int b = 7;
         int c = 10;
-        if (a + b > c & a + c > b && b + c > a) {
+        if (a + b > c && a + c > b && b + c > a) {
             System.out.println("Такой треугольник существует");
         } else {
             System.out.println("Такого треугольника не существует");
