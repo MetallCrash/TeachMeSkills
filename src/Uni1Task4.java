ad6ed75c090a7a3dcddc8c4c095c65e21f2c548e
Kostya Kalevich <kostyadante@tut.by>
2021-07-09 17:46:55 +0300

    Finished Uni1Task4

--- src/Uni1Task4.java
+++ src/Uni1Task4.java
@@ -1,18 +1,18 @@
 public class Uni1Task4 {
-    public static void main(String[]args){
-        int a=5;
-        int b=0;
-        int c=-9;
-        int pos=0;
-        if (a>0) {
-            pos++;
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 0;
+        int c = -9;
+        int positiveCount = 0;
+        if (a > 0) {
+            positiveCount++;
         }
         if (b > 0) {
-           pos++;
+            positiveCount++;
         }
-        if (c>0){
-            pos++;
+        if (c > 0) {
+            positiveCount++;
         }
-        System.out.println("Количество положительных чисел: "+pos);
+        System.out.println("Количество положительных чисел: " + positiveCount);
     }
 }
