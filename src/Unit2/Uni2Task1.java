9e23b6b4263d63c71b1365b7330f5957242ce458
Kostya Kalevich <kostyadante@tut.by>
2021-07-13 16:08:26 +0300

    Finished Uni2Task1

--- src/Unit2/Uni2Task1.java
+++ src/Unit2/Uni2Task1.java
@@ -2,14 +2,14 @@ package Unit2;
 
 public class Uni2Task1 {
     public static void main(String[] args) {
-        double distance = 10;
-        double distanceNext;
-        double sumDistance=0;
-        for (int day=1;day<7;day++){
-            sumDistance+=distance;
-            distanceNext=distance/10;
-            distance+=distanceNext;
+        double distance = 10; // расстояние первого дня
+        double distanceNext; // задаем переменную разницы
+        double sumDistance = 0; // общая дистанция
+        for (int day = 1; day < 7; day++) {
+            sumDistance += distance; // к общей дистанции добавляем дистанцию за текущий день
+            distanceNext = distance / 10; // результат разницы последующего дня с текущим
+            distance += distanceNext; // планируемая дистанция на следующий день
         }
-        System.out.println(sumDistance);
+        System.out.println("Дистанция за 7 дней: " + sumDistance + " км");
     }
 }
