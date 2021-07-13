5c61a555e6f1d3e91c2d0568abcf92194a7fe692
Kostya Kalevich <kostyadante@tut.by>
2021-07-13 17:04:46 +0300

    Finished Uni2Task6

---
+++ src/Unit2/Uni2Task6.java
@@ -0,0 +1,11 @@
+package Unit2;
+
+public class Uni2Task6 {
+    public static void main(String[] args) {
+        int a = 2;
+        int sum;
+        for (sum = 2; sum <= 100; sum += a) {
+            System.out.print(sum + " ");
+        }
+    }
+}
