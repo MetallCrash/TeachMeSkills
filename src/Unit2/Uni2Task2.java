a82895649120ab72e84acf6261fbb4e42c807d70
Kostya Kalevich <kostyadante@tut.by>
2021-07-13 13:01:59 +0300

    Finished Uni2Task2

---
+++ src/Unit2/Uni2Task2.java
@@ -0,0 +1,16 @@
+package Unit2;
+
+public class Uni2Task2 {
+    public static void main (String[] args) {
+        int time = 3;
+        int amoeba = 2;
+        while(time<25)
+
+        {
+            System.out.println("Количество амеб через "+time+" часов: " + amoeba);
+            amoeba *= 2;
+            time += 3;
+        }
+
+    }
+}
