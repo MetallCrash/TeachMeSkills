c9c4392df2a2c86c8d5d14422d3a0ef3938ff99a
Kostya Kalevich <kostyadante@tut.by>
2021-07-13 17:09:57 +0300

    Finished Uni2Task6

--- src/Unit2/Uni2Task6.java
+++ src/Unit2/Uni2Task6.java
@@ -4,7 +4,7 @@ public class Uni2Task6 {
     public static void main(String[] args) {
         int a = 2;
         int num;
-        for (num = 2; num <= 100; num += a) {
+        for (num = 2; num <= 100; num += 2) {
             System.out.print(num + " ");
         }
     }
