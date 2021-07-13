f3dc8cdab9bb40c4ecc2a2e472b67bf6ac82391e
Kostya Kalevich <kostyadante@tut.by>
2021-07-13 11:36:51 +0300

    Finished Uni2Task0

--- src/Unit2/Uni2Task0.java
+++ src/Unit2/Uni2Task0.java
@@ -2,20 +2,20 @@ package Unit2;
 
 public class Uni2Task0 {
     public static void main(String[] args) {
-        int a=0;
-        while (a<11){
-            System.out.print(a+" ");
+        int a = 0;
+        while (a < 11) {
+            System.out.print(a + " ");
             a++;
         }
         System.out.println();
-        a=0;
+        a = 0;
         do {
-            System.out.print(a+" ");
+            System.out.print(a + " ");
             a++;
-        }while (a<11);
+        } while (a < 11);
         System.out.println();
-        for(a=0;a<11;a++){
-            System.out.print(a+" ");
+        for (a = 0; a < 11; a++) {
+            System.out.print(a + " ");
         }
     }
 }
