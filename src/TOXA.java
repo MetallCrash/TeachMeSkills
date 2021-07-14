23c8bf602d5e6c27918e7b96c684ab541af4aaed
Kostya Kalevich <kostyadante@tut.by>
2021-07-14 12:21:07 +0300

    'Unit2'

---
+++ src/TOXA.java
@@ -0,0 +1,24 @@
+public class TOXA {
+    public static void main(String[] args) {
+        int a = -0;
+        if (a > 0) {
+            if (a < 10) {
+                System.out.println(a + " является однозначным положительным числом");
+            } else if (a >= 10 && a < 100) {
+                System.out.println(a + " является двухзначным положительным числом");
+            } else {
+                System.out.println(a + " является трехзначным и более положительным числом");
+            }
+        } else if (a < 0) {
+            if (a > -10) {
+                System.out.println(a + " является однозначным отрицательным числом");
+            } else if (a <= -10 && a > -100) {
+                System.out.println(a + " является двухзначным отрицательным числом");
+            } else {
+                System.out.println(a + "  является трехзначным и более отрицательным числом");
+            }
+        } else {
+            System.out.println(a + " не является ни положительным, ни отрицательным числом, однозначное числом");
+        }
+    }
+}
