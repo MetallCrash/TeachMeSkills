4b2fec59b67bcde42e117bc52cdf29738f9a53cf
Kostya Kalevich <kostyadante@tut.by>
2021-07-17 19:06:24 +0300

    Finished Uni3Task14

--- src/Unit3/Uni3Task14.java
+++ src/Unit3/Uni3Task14.java
@@ -11,34 +11,20 @@ public class Uni3Task14 {
         int sc = scanner.nextInt();
         int[] arr = new int[sc];
         for (int i = 0; i < arr.length; i++) {
-            int r = random.nextInt(100);
+            int r = random.nextInt(sc);
             arr[i] = r;
             System.out.print(arr[i] + " ");
         }
-        int min;
-        if (arr[0] != 0) {
-            min = arr[0];
-            for (int i = 0; i < arr.length; i++) {
-                if (arr[i] % 2 != 0) {
-                    if (min > arr[i]) {
-                        min = arr[i];
-                    }
+        int min = sc;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) {
+                if (min > arr[i]) {
+                    min = arr[i];
                 }
             }
-            System.out.println();
-            System.out.println("Минимальный нечетный элемент массива: " + min);
-        } else {
-            min = arr[1];
-            for (int i = 1; i < arr.length; i++) {
-                if (arr[i] % 2 != 0) {
-                    if (min > arr[i]) {
-                        min = arr[i];
-                    }
-                }
-            }
-            System.out.println();
-            System.out.println("Минимальный нечетный элемент массива: " + min);
         }
+        System.out.println();
+        System.out.println("Минимальный нечетный элемент массива: " + min);
     }
 }
 
