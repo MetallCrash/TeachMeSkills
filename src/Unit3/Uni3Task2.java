1a4953aadd5e9882f05eda516721cee0369a1df9
Kostya Kalevich <kostyadante@tut.by>
2021-07-17 16:30:54 +0300

    Finished Uni3Task2

--- src/Unit3/Uni3Task2.java
+++ src/Unit3/Uni3Task2.java
@@ -3,17 +3,14 @@ package Unit3;
 public class Uni3Task2 {
     public static void main(String[] args) {
         int[] mas = new int[50];
-        int min = 1;
-        int max = 99;
-        for (int i = 0; i < 50; i++) {
-            mas[i] = min;
-            min += 2;
+        int n = 1;
+        for (int i = 0; i < mas.length; i++) {
+            mas[i] = n;
+            n += 2;
             System.out.print(mas[i] + " ");
         }
         System.out.println();
-        for (int i = 49; i >= 0; i--) {
-            mas[i] = max;
-            max -= 2;
+        for (int i = mas.length-1; i >= 0; i--) {
             System.out.print(mas[i] + " ");
         }
     }
