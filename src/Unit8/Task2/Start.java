144383e2e8b98141d81dbd045ac6e693f0295d5e
Kostya Kalevich <kostyadante@tut.by>
2021-08-06 17:51:32 +0300

    Finished Unit8

--- src/Unit8/Task2/Start.java
+++ src/Unit8/Task2/Start.java
@@ -2,14 +2,12 @@ package Unit8.Task2;
 
 public interface Start {
     boolean checkingSystem();
-    default void engineStart(){
-        int [] count=new int[10];
-        int number=10;
-        for (int i = 0; i<count.length;i++){
-            count[i]=number;
-            System.out.println(count[i]+"...");
-            number--;
+
+    void start();
+
+    default void engineStart() {
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i + "...");
         }
     }
-    void start();
 }
